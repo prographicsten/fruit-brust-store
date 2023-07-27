549882e3556f5916bd3a1ee821ce416c44ea6b8c
prographicsten <prographicsten@gmail.com>
2023-07-27 20:53:40 +0600

    some change again

--- index.js
+++
@@ -1,8 +0,0 @@
-let popup = document.getElementById('popup');
-
-function openPopup() {
-    popup.classList.toggle('popup-open');
-}
-function closePopup() {
-    popup.classList.remove('popup-open');
-}
